2ca4a2a97a829670d55a7e2b72574f7456afb1db
ShuMoo789 <shuvuive789@gmail.com>
2024-01-24 11:33:25 +0700

    Component Part 1

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Footer.css'; 
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div>
+          <p>copyright © 2022</p>
+        </div>
+      </footer>
+    )
+  }
+}
